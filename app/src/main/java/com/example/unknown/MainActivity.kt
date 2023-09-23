0b9f335e271a832fa3edc4e5934cb48bf0e031c8
Monica Villanueva <vllnv.monica@gmail.com>
2023-09-23 22:51:28 +0800

    learn it first interface layout

--- app/src/main/java/com/example/unknown/MainActivity.kt
+++ app/src/main/java/com/example/unknown/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.unknown
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var ImageView: ImageView
+//    private lateinit var Drawable: Drawable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//
+//        ImageView = findViewById(R.id.imageView)
+//        Drawable = resources.getDrawable(R.drawable.logoname)
     }
 }
