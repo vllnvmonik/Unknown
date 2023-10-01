a9379fad8cbc8ff8a3ab78b09fa2bca9f126748e
Monica Villanueva <vllnv.monica@gmail.com>
2023-10-01 19:11:09 +0800

    created the profile interface under login activity and currently coding services

--- app/src/main/java/com/example/unknown/MainActivity.kt
+++ app/src/main/java/com/example/unknown/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.unknown
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 //    private lateinit var ImageView: ImageView
 //    private lateinit var Drawable: Drawable
+        private lateinit var startBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //
 //        ImageView = findViewById(R.id.imageView)
 //        Drawable = resources.getDrawable(R.drawable.logoname)
+        startBtn = findViewById(R.id.startBtn)
+
+        startBtn.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/unknown/login.kt
+++ app/src/main/java/com/example/unknown/login.kt
@@ -1,11 +1,26 @@
 package com.example.unknown
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlin.math.log
 
 class login : AppCompatActivity() {
+    private lateinit var logoutBtn: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        logoutBtn = findViewById(R.id.logoutBtn)
+
+        logoutBtn.setOnClickListener {
+            val intent = Intent(this, services::class.java)
+            startActivity(intent)
+        }
+
     }
 }
